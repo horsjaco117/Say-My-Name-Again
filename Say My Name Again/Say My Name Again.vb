918c4d4e879aa2a1a912a362bb75e917e78c2f3e
horsjaco117 <jacob_horsley@live.com>
2025-02-01 17:47:23 -0700

    Say my name with modifications made for customized responses

---
+++ Say My Name Again/Say My Name Again.vb
@@ -0,0 +1,39 @@
+﻿'Jacob Horsley
+'Spring 2025
+'RCET 2265
+'SayMyName
+
+Option Explicit On
+Option Strict On
+
+Module Module1
+
+
+
+    Sub Main()
+
+        Dim userInput As String
+
+        Console.WriteLine("What is your Name?") 'Ask name
+        Console.WriteLine()
+
+        userInput = Console.ReadLine() 'give it name
+
+        If userInput = "Jacob" Then
+            Console.WriteLine("Hello There Handsome!")
+        ElseIf userInput = "Emily" Then
+            Console.WriteLine("How's it going Emily?")
+        ElseIf userInput = "Joe" Then
+            Console.WriteLine("Joe Mama!!!")
+        Else
+            Console.WriteLine()
+            Console.WriteLine("Hello there " & userInput)
+        End If
+
+        Console.WriteLine()
+
+
+
+    End Sub
+
+End Module
